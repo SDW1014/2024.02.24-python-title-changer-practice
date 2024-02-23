b296001e01583ca437cdc7f9067f782184c23f52
SDW1014 <apif730@gmail.com>
2024-02-24 02:10:53 +0900

    [feat] main 함수 사용 개선 - 새로 만들어진 함수를 사용하는 부분을 만듬

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-
+import titleChanger
 
 def main():
-    print("Hello")
-    
+    ## 기본 시작 함수 ## 
+    # print("Hello")
+    # print(titleChanger.extract_and_modify_string(''))
+    # print(titleChanger.extract_FC2PPVNUMBER(""))
+    print(titleChanger.extract_perpect(''))
+
 if __name__ == "__main__":
     main()
